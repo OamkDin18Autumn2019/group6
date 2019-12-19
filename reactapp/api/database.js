f94573adf2f47ac456f5d4f54e3021fc917d4272
Mafas Dhaimash <mafas@hotmail.fi>
2019-12-19 16:15:57 +0200

    added db and design

--- reactapp/api/database.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'library'
-});
-module.exports = connection;
