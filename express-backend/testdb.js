3dbc8aaaf86e38b92e2362b747ebb7783bb78cc7
Mafas Dhaimash <mafas@hotmail.fi>
2019-12-19 16:06:49 +0200

    added db and design

--- express-backend/testdb.js
+++
@@ -1,6 +0,0 @@
-let books = [
-    { _id: 1, name: "Harry Potter and the Philosopher's Stone", author: "J. K. Rowling" },
-    { _id: 2, name: "Math123", author: "Matt Thompson" },
-    { _id: 3, name: "Animals", author: "Jaakko Lampi" }
-  ];
-  module.exports = books;
